bc6f4c255faf2cc1ed8fa3c2b187263a0b455686
MakesCode <makescodeur@gmail.com>
2022-07-10 22:46:29 +0200

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,11 @@ import Home from './page/Home';
 
 
 function App() {
- 
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,18 +7,18 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 // eslint-disable-next-line no-undef
 if (process.env.NODE_ENV === 'development') {
-      // eslint-disable-next-line no-undef
-      const { worker } = require('./service/msw/browser')
-      worker.start()
+  // eslint-disable-next-line no-undef
+  const { worker } = require('./service/msw/browser')
+  worker.start()
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <Provider store={store}>
-            <BrowserRouter>
-                  <App />
-            </BrowserRouter>
-      </Provider>
- 
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+
 );
 
